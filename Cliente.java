d31a58a24ee67b28c5280d83054f217a52e71ec9
Carlosdsa2005 <160856690+Carlosdsa2005@users.noreply.github.com>
2025-03-12 10:47:38 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+package com.mycompany.mavenproject1;
+
+
+public class Cliente {
+        String nome;
+        String sobrenome;
+        String cpf;
+    
+        public Cliente(String nom,String sobre,String cpf_cli){
+            this.nome = nom;
+            sobrenome = sobre;
+            cpf = cpf_cli;
+        }
+}
