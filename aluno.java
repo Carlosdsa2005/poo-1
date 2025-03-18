1c922a9ea224969edd080466651d0d5b88d75c1e
Carlosdsa2005 <160856690+Carlosdsa2005@users.noreply.github.com>
2025-03-18 10:57:11 -0300

    Create aluno.java

---
+++ aluno.java
@@ -0,0 +1,33 @@
+package com.mycompany.uvv;
+
+
+
+public class aluno {
+    public static int qtdalunos = 0 ;
+    private String matricula;
+    public String nome;
+    public String curso;
+    public String turma;
+    int periodo;
+    double prime_bimes;
+    double segun_bimeste;
+    
+
+    public aluno(String n,String c){
+        this.nome =  n;
+        nome = toUpperCase(nome);
+        this.curso = c;
+        this.turma = "A";
+        this.periodo = 1;
+
+}
+    public aluno(int ano ){
+       
+    }
+    public void Setmatricula(){
+        
+    }
+
+
+     
+}
