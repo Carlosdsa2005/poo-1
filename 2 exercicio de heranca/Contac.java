127b6fe1674597bceb1a3a3128283f9f4540910f
Carlosdsa2005 <160856690+Carlosdsa2005@users.noreply.github.com>
2025-05-20 10:49:30 -0300

    Update Contac.java

--- 2 exercicio de heranca/Contac.java
+++ 2 exercicio de heranca/Contac.java
@@ -6,14 +6,15 @@ class Contac extends Conta{
  
 
     public Contac(double saldo) {
-         super(saldo);
+        super(saldo);
     }
+
 
     @Override
      public void sacar(double v){
-        if ((v + 15) <= getSaldo()){
-            setSaldo(getSaldo() - (v + 15));
-            System.err.println("SAQUE FEITO ; novo saldo da conta poupanca: " + getSaldo());
+        if ((v + 5) <= getSaldo()){
+            setSaldo(getSaldo() - (v + 5));
+            System.err.println("SAQUE FEITO ; novo saldo da conta corrente: " + getSaldo());
         }
         else{
             System.err.println("saldo insuficente");
@@ -24,11 +25,11 @@ class Contac extends Conta{
     @Override
     public void depositar(double v){
         if( v > 0){
-             setSaldo(getSaldo() + v); 
-             System.err.println("DEPOSITO REALIZADO ; novo saldo da conta poupanca : " + getSaldo());         
+             setSaldo(getSaldo() + v);
+             System.err.println("DEPOSITO REALIZADO ; novo saldo da conta corrente : " + getSaldo());
         }
         else {
-           System.err.println("valor nao aceito");
+            System.err.println("saldo insuficente");
 
         }
     }
@@ -36,11 +37,9 @@ class Contac extends Conta{
     
     @Override
     public void rendimento(){
-        setSaldo(getSaldo() + (getSaldo() * 0.05));
-        System.err.println("RENDIMENTO APLICADO; novo saldo da conta poupanca: " + getSaldo());
+        setSaldo(getSaldo() +(getSaldo() * 0.02));
+        System.err.println("RENDIMENTO APLICADO; novo saldo da conta corrente: " + getSaldo());
     }
-    }
-    
     
    
     
